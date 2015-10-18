5fd901a96137e521e502a30e27731e72dac84d7b
sc630@kent.ac.uk <sc630@kent.ac.uk>
2015-10-18 20:06:21 +0100

    First commit

---
+++ NoiseDB/NoiseDB/DiskService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoiseDB
+{
+    class DiskService
+    {
+        public DiskService()
+        {
+
+        }
+
+        public void OutputHashMapToDisk(HashSet<int> hashMap)
+        {
+
+        }
+
+        public HashSet<int> ReadHashMapFromFile(string path)
+        {
+            return null;
+        }
+    }
+}
